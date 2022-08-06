ab05cd0771e2363df046791e7e3d6542b2763654
SJasonHumphrey <sjasonhumphrey@gmail.com>
2022-08-06 13:36:00 -0400

    added sidebar and routes

--- src/App.js
+++ src/App.js
@@ -5,19 +5,58 @@ import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 const App = () => {
+  const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
             <TooltipComponent content="Settings" position="Top">
-              <button type="button" 
-              style={{ background: "blue", borderRadius: "50%"}}
-              className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray">
+              <button
+                type="button"
+                style={{ background: "blue", borderRadius: "50%" }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
                 <FiSettings />
               </button>
             </TooltipComponent>
           </div>
+          {activeMenu ? (
+            <div
+              className="w-72 fixed sidebar
+            dark:bg-secondary-dark-bg
+            bg-white"
+            >
+              Sidebar
+            </div>
+          ) : (
+            <div
+              className="w-0 
+            dark:bg-secondary-dark-bg"
+            >
+              Sidebar
+            </div>
+          )}
+          <div
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              Navbar
+            </div>
+          </div>
+          <Routes>
+            {/* Dashboard */}
+            <Route path="/" element="ECommerce" />
+            <Route path="/ecommerce" element="ECommerce" />
+            {/* Pages */}
+            <Route path="/orders" element="Orders" />
+            <Route path="/employees" element="Employees" />
+            <Route path="/customers" element="Customers" />
+            {/* Apps */}
+          </Routes>
         </div>
       </BrowserRouter>
     </div>
